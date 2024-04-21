5805732583661d7bbceef0925502a040585c93f1
Nirmalya Nayak <n.nayak263@gmail.com>
2024-04-21 09:31:03 +0530

    add shadcn-ui/sonner for toast

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,31 +5,16 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs";
-import { toast } from "sonner";
 
 import { runTask } from "@/actions/aws";
 
 export default async function DashboardPage() {
-  const user = await currentUser();
-  const [isPending, startTransition] = useTransition();
-
-  const runTaskButton = () => {
-    startTransition(() => {
-      runTask()
-        .then(() => {
-          toast.success("Connection generated");
-        })
-        .catch(() => toast.error("Connection generation failed"));
-    });
-  };
-
   return (
     <div>
       <h1>DashboardPage (Protected)</h1>
       <SignedIn>
         <UserButton afterSignOutUrl="/" />
-        <button className="text-4xl font-bold" onClick={runTaskButton}>
+        <button className="text-4xl font-bold" onClick={runTask}>
           Run Task
         </button>
       </SignedIn>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 import "./globals.css";
 
@@ -19,7 +20,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <Toaster />
+        </body>
       </html>
     </ClerkProvider>
   );
