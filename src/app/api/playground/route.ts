56d9b52d70fcf244bf7ba08a248fbf38f802ce9e
Nirmalya Nayak <n.nayak263@gmail.com>
2024-04-27 21:14:15 +0530

    sync local dir to s3 bucket

--- src/app/api/playground/route.ts
+++ src/app/api/playground/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
+import { S3Client, CreateBucketCommand } from "@aws-sdk/client-s3";
+import { S3SyncClient } from "s3-sync-client";
 
 import db from "@/db/drizzle";
 import { project } from "@/db/schema";
@@ -27,7 +29,34 @@ export async function POST(req: Request) {
       })
       .returning();
 
-    return NextResponse.json({ data: newProject[0] });
+    const projectCreated = newProject[0];
+
+    const bucketName = projectCreated.id;
+
+    const s3Client = new S3Client({
+      region: process.env.AWS_REGION!,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+    });
+    const { sync } = new S3SyncClient({ client: s3Client });
+
+    try {
+      await s3Client.send(
+        new CreateBucketCommand({
+          Bucket: bucketName,
+        })
+      );
+
+      await sync("s3://react-vite-base", `s3://${bucketName}`);
+    } catch (error: any) {
+      if (error["$metadata"].httpStatusCode === 409) {
+        console.error("Bucket has been created already");
+      }
+    }
+
+    return NextResponse.json({ data: projectCreated });
   } catch (error) {
     console.error("[PLAYGROUND_POST", error);
     return new NextResponse("Internal Error", { status: 500 });
